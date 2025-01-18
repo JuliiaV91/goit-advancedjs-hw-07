530677c558eac3ed57ef18600be78221566d9947
IULIIA VOSKOBOINYK <juliia.whatever@gmail.com>
2025-01-18 22:47:02 +0100

    1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,59 @@
+
+class Key {
+    private signature: number;
+
+    constructor(signature: number = Math.random()) {
+        this.signature = signature;
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log(`Welcome ${person.getKey().getSignature()}`);
+        }
+    } 
+
+    abstract openDoor(key: Key): void;
+        
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('Welcome!');
+        } else {
+            console.log('Sorry, key does not match');
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +63,4 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
-
-export {};
+export { };
